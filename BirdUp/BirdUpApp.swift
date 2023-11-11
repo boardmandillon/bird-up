ebd970cf9f9544bf0086b15a9d123ce4549d8891
Dillon Boardman <iamdillonboardman@gmail.com>
2023-11-11 18:59:24 +0000

    hello world

---
+++ BirdUp/BirdUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdUpApp.swift
+//  BirdUp
+//
+//  Created by Dillon Boardman on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BirdUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
